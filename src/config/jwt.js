c5bd6ba5f953cdce2045f470c4488950106bfc3b
Alex Lu <siralexluweb@gmail.com>
2025-10-03 13:37:45 +0530

    Create jwt.js

---
+++ src/config/jwt.js
@@ -0,0 +1,10 @@
+// src/config/jwt.js
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+module.exports = { generateToken };
