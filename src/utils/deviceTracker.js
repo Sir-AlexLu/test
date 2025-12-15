101494ec721f248bca54e3eb9a636ca09b5724c9
Alex Lu <siralexluweb@gmail.com>
2025-12-15 11:22:11 +0530

    Delete src directory

--- src/utils/deviceTracker.js
+++
@@ -1,8 +0,0 @@
-// src/utils/deviceTracker.js
-const crypto = require('crypto');
-
-const generateDeviceId = () => {
-  return crypto.randomBytes(16).toString('hex');
-};
-
-module.exports = { generateDeviceId };
